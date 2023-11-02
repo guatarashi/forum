44e1b96e195b911febc4a68617d125b43c94b34e
Gustavo <guatarashi@gmail.com>
2023-11-02 09:38:47 -0300

    feat: Aula 2 - Authenticando usuário.

--- src/main/kotlin/br/com/atarashi/forum/config/SecurityConfiguration.kt
+++ src/main/kotlin/br/com/atarashi/forum/config/SecurityConfiguration.kt
@@ -2,46 +2,40 @@ package br.com.atarashi.forum.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.core.userdetails.User
-import org.springframework.security.core.userdetails.UserDetailsService
-import org.springframework.security.provisioning.InMemoryUserDetailsManager
+import org.springframework.security.config.annotation.web.invoke
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
-
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfiguration(
-    private val configuration: AuthenticationConfiguration
-) {
+class SecurityConfiguration {
 
-//    https://spring.io/guides/gs/securing-web/
     @Bean
-    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
-        http.authorizeHttpRequests { requests ->
-            requests.requestMatchers("/topicos").permitAll()
-                .anyRequest()
-                .authenticated()
-        }
-            .formLogin { form ->
-                form.loginPage("/login").permitAll()
+    open fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http {
+            authorizeHttpRequests {
+                authorize("/topicos", hasAuthority("LEITURA_ESCRITA"))
+                authorize(anyRequest, authenticated)
             }
-            .logout { logout -> logout.permitAll() }
+            sessionManagement {
+                sessionCreationPolicy = SessionCreationPolicy.STATELESS
+            }
 
+            httpBasic {  }
 
+            formLogin { }
+
+            logout {  }
+        }
         return http.build()
     }
 
     @Bean
-    fun userDetailsService(): UserDetailsService {
-        val user = User.withDefaultPasswordEncoder()
-            .username("user")
-            .password("password")
-            .roles("USER")
-            .build()
-
-        return InMemoryUserDetailsManager(user)
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
     }
 }
---
+++ src/main/kotlin/br/com/atarashi/forum/model/Role.kt
@@ -0,0 +1,19 @@
+package br.com.atarashi.forum.model
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import org.springframework.security.core.GrantedAuthority
+
+@Entity
+data class Role(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private val id: Long,
+    private val nome: String
+): GrantedAuthority {
+
+    override fun getAuthority() = nome
+
+}
